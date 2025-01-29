8587f258ad2caefbad763ff3e068d9bd03521263
Nicolas <nicobava96@gmail.com>
2025-01-29 13:02:57 -0600

    Fix errors 2

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ChatBox from './components/ChatBox';
+// import ChatBox from './components/ChatBox';
 import ResearchPage from './pages/Research';
 import LeaderboardPage from './pages/LeaderBoard';
 import InfluencerDetailPage from './pages/InfluencerDetail';
@@ -9,7 +9,7 @@ const AppRoutes = () => {
     <Router>
       <Routes>
         <Route path="/research" element={<ResearchPage />} />
-        <Route path="/chatbox" element={<ChatBox />} />
+        {/* <Route path="/chatbox" element={<ChatBox />} /> */}
         <Route path="/leaderboard" element={<LeaderboardPage />} />
         <Route path="/detail" element={<InfluencerDetailPage />} />
       </Routes>
