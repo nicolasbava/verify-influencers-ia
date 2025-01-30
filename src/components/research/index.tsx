5dc239b03d484be49a99b3013a39bb670c038902
Nicolas <nicobava96@gmail.com>
2025-01-30 01:56:11 -0600

    added link back to leaderboard

--- src/components/research/index.tsx
+++ src/components/research/index.tsx
@@ -1,15 +1,18 @@
 import { Box, Typography } from "@mui/material";
 import ResearchForm from "./ResearchForm";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link } from "react-router-dom";
 
 const ResearchComponent = () => {
     return (
         <Box sx={{width: '60vw'}}>
-            <Box sx={{display: 'flex', mb: 2, alignItems: 'center'}}>
-                <ArrowBackIcon sx={{color: 'secondary.light', mr: 1}} /> 
-                <Typography mr={2} sx={{color: 'secondary.light'}}>Back to LeaderBoard</Typography>
-                <Typography variant="h5" fontWeight='bold'>Research Tasks</Typography>
-            </Box>
+            <Link to='/leaderboard'>
+                <Box sx={{display: 'flex', mb: 2, alignItems: 'center'}}>
+                    <ArrowBackIcon sx={{color: 'secondary.light', mr: 1}} /> 
+                    <Typography mr={2} sx={{color: 'secondary.light'}}>Back to LeaderBoard</Typography>
+                    <Typography variant="h5" fontWeight='bold'>Research Tasks</Typography>
+                </Box>
+            </Link>
             <ResearchForm />
         </Box>
     )
